fd6c96480209ec50bba99690fd1f4dde876ee972
Nazar Khlivnyi <135509585+NazarKhl@users.noreply.github.com>
2024-11-21 10:24:12 +0100

    Add decoration to home page

--- personalPortfolio/src/App.jsx
+++ personalPortfolio/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="homePage">
+        <div className="navBar">
+          <div className="navBarElement icon">
+            About<span className="mePart">Me.</span>
+          </div>
+          <div className="navbarRightEl">
+            <a className="navBarElements home" href="#homePage">HOME</a>
+            <a className="navBarElements about"  href="">ABOUT</a>
+            <a className="navBarElements portfolio" href="">PORTFOLIO</a>
+            <a className="navBarElements resume" href="">RESUME</a>
+            <a className="navBarElements contact" href="">CONTACT</a>
+          </div>
+        </div>
+        <div className="imgBox"></div>
+        <div className="shortInfo">
+          <p className="nameWord">
+            <span className="helloWord">Who</span> I am?
+          </p>
+          <p className="personalInfo">
+            Frontend Developer skilled in React.js, Next.js, TypeScript, <br />
+            and SQL, with experience in building responsive UIs, designing{" "}
+            <br />
+            RESTful APIs, and optimizing backend performance. Strong
+            understanding
+            <br /> of frontend and backend integration, delivering secure,
+            scalable web applications.
+          </p>
+          <br />
+          <button className="contactButton">Contact me</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id="aboutPage" className="scrollableSection">
+        <p>Some data</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
